56d961b7250b45b8e7fffd7e9287bc0ed0c3f7f1
Sean Pines <spines@vmware.com>
2023-11-28 14:43:15 -0500

    Updates to trigger new supply chain

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello from the server! This is an update!');
 });
 
 app.get('/health', (req, res) => {
