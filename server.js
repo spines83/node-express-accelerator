412affa0f1a6030a5ca8e43fc782c7567769ac56
Sean Pines <spines@vmware.com>
2023-11-30 14:00:52 -0500

    triggering ci

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server! This is an update!');
+  res.send('Hello from the server! This is an update! Again!');
 });
 
 app.get('/health', (req, res) => {
